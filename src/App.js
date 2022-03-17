e8e4dc97953c3a0055943375878b8b7185ac45fe
Lee Naeun <nanni4287@gmail.com>
2022-03-17 22:48:57 +0900

    구인공고 css 완성

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {
   PostingUl,
   PostingBox,
   PostingImg,
+  PostingBoxFirst,
+  PostingBoxSecond,
+  PostingBoxThird,
 } from './style/styledPosting';
 
 function App() {
@@ -12,16 +15,81 @@ function App() {
     <div className='App'>
       <PostingWrapBox>
         <PostingUl>
+          {/* PostingLi 반복해주면 됨 */}
           <PostingLi>
             <span>채용 공고 카드</span>
             <PostingBox>
               <ul>
-                <li id='boxFirst'></li>
-                <li id='boxSecond'></li>
-                <li id='boxThird'></li>
+                <PostingBoxFirst></PostingBoxFirst>
+                <PostingBoxSecond>
+                  <div>
+                    <h3>인디밴드 앨범 자켓 그래픽 디자이너 구인(2년차 이상)</h3>
+                    <span>인디인디 엔터</span>
+                  </div>
+                  <div>
+                    <div id='tag'>
+                      Tag: <span>Design</span>
+                      <span>Graphic</span>
+                    </div>
+                    <div id='country'>Korea, Republic of</div>
+                  </div>
+                </PostingBoxSecond>
+                <PostingBoxThird>
+                  <div id='bookmark'></div>
+                  <div id='title'>
+                    <p>급여</p>
+                    <p>채용 기간</p>
+                    <p>지원자</p>
+                  </div>
+                  <div id='descript'>
+                    <p>추후 협의</p>
+                    <p>D- 14일(2022-04-05)</p>
+                    <p>
+                      12명 <span>15T03:58:51:5792</span>
+                    </p>
+                  </div>
+                </PostingBoxThird>
               </ul>
             </PostingBox>
           </PostingLi>
+          {/* PostingLi 반복해주면 됨 */}
+          <PostingLi>
+            <span>채용 공고 카드</span>
+            <PostingBox>
+              <ul>
+                <PostingBoxFirst></PostingBoxFirst>
+                <PostingBoxSecond>
+                  <div>
+                    <h3>인디밴드 앨범 자켓 그래픽 디자이너 구인(2년차 이상)</h3>
+                    <span>인디인디 엔터</span>
+                  </div>
+                  <div>
+                    <div id='tag'>
+                      Tag: <span>Design</span>
+                      <span>Graphic</span>
+                    </div>
+                    <div id='country'>Korea, Republic of</div>
+                  </div>
+                </PostingBoxSecond>
+                <PostingBoxThird>
+                  <div id='bookmark'></div>
+                  <div id='title'>
+                    <p>급여</p>
+                    <p>채용 기간</p>
+                    <p>지원자</p>
+                  </div>
+                  <div id='descript'>
+                    <p>추후 협의</p>
+                    <p>D- 14일(2022-04-05)</p>
+                    <p>
+                      12명 <span>15T03:58:51:5792</span>
+                    </p>
+                  </div>
+                </PostingBoxThird>
+              </ul>
+            </PostingBox>
+          </PostingLi>
+          {/* PostingLi 반복해주면 됨 */}
         </PostingUl>
       </PostingWrapBox>
     </div>
--- src/style/styledPosting.js
+++ src/style/styledPosting.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import '../App.css';
 
 export const PostingWrapBox = styled.div`
-  background-color: red;
+  background-color: #dcdcdc;
   height: 100vh;
   display: flex;
   justify-content: center;
@@ -10,7 +11,6 @@ export const PostingWrapBox = styled.div`
 
 export const PostingUl = styled.ul`
   width: 1100px;
-  background-color: orange;
   > li:nth-child(1) {
     margin-top: 0;
   }
@@ -19,7 +19,6 @@ export const PostingUl = styled.ul`
 export const PostingLi = styled.li`
   width: 1100px;
   margin-top: 30px;
-  background-color: pink;
   > span {
     font-size: 13px;
   }
@@ -35,15 +34,86 @@ export const PostingBox = styled.div`
     height: 140px;
     display: grid;
     grid-template-columns: 140px 600px 300px;
-    background-color: orange;
     > li {
       margin-left: 10px;
-      background-color: green;
     }
     > li:nth-child(1) {
       margin-left: 0;
+      border-radius: 10px;
+      background-color: gray;
     }
   }
 `;
 
-export const PostingImg = styled.div``;
+export const PostingBoxFirst = styled.li`
+  /* background-image 넣으면 됨 */
+`;
+
+export const PostingBoxSecond = styled.li`
+  > div:nth-child(1) {
+    height: 85px;
+    > h3 {
+      font-size: 20px;
+    }
+    > span {
+      font-size: 14px;
+      color: gray;
+      font-weight: bold;
+    }
+  }
+  > div:nth-child(2) {
+    > #tag {
+      height: 35px;
+      color: gray;
+      > span {
+        width: 80px;
+        height: 25px;
+        display: inline-block;
+        background-color: #264d84;
+        margin-left: 10px;
+        border-radius: 20px;
+        text-align: center;
+        line-height: 25px;
+        color: #fff;
+        font-size: 12px;
+      }
+    }
+    > #country {
+      color: gray;
+    }
+  }
+`;
+
+export const PostingBoxThird = styled.li`
+  display: grid;
+  grid-template-columns: 40px 80px 180px;
+  color: gray;
+  > div {
+    font-size: 14px;
+    > p {
+      padding-top: 20px;
+    }
+    /* margin-left: 10px; */
+  }
+  > #bookmark {
+    width: 20px;
+    height: 20px;
+    background-color: red;
+  }
+  > #title {
+    > p:nth-child(1) {
+      font-weight: bold;
+    }
+  }
+  > #descript {
+    text-align: right;
+    > p:nth-child(1) {
+      font-weight: bold;
+    }
+    > p:nth-child(3) {
+      span {
+        display: block;
+      }
+    }
+  }
+`;
